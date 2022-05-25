86636f9cee6874020e580039a3a5252ec0ad60b7
kurucaymurat7 <100161987+kurucaymurat7@users.noreply.github.com>
2022-05-25 19:13:05 +0300

    murat commit1

--- src/test/java/package01/C01.java
+++ src/test/java/package01/C01.java
@@ -2,7 +2,7 @@ package package01;
 
 public class C01 {
     public static void main(String[] args) {
-        System.out.println("proje");
+        System.out.println("murat 1");
+        System.out.println("murat 2");
     }
 }
-
