9a0c8e1ac16105e2a2f60766396430e9649bef06
kurucaymurat7 <100161987+kurucaymurat7@users.noreply.github.com>
2022-05-25 19:17:16 +0300

    murat commit3

--- src/test/java/package01/C01.java
+++ src/test/java/package01/C01.java
@@ -2,6 +2,7 @@ package package01;
 
 public class C01 {
     public static void main(String[] args) {
+        System.out.println("murat en son ekleme");
         System.out.println("murat 1");
         System.out.println("murat 2");
     }
